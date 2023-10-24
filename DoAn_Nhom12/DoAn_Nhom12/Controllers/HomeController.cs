cc5d2c6594ea1c99d3ab78b7839f3244a908824a
nguyenquocthai1463 <115343560+nguyenquocthai1463@users.noreply.github.com>
2023-10-24 17:49:09 +0700

    24/10

---
+++ DoAn_Nhom12/DoAn_Nhom12/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_Nhom12.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TrangDiemMoi()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DoAn_Nhom12/DoAn_Nhom12/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoAn_Nhom12.MvcApplication" Language="C#" %>
